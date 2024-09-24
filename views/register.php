c8a80d22b19ec67c4c5e4fb50982213184e3410a
Danilo Felype <danilo87651@gmail.com>
2024-09-24 11:46:17 -0300

    finaliza etapa backend do projeto

--- views/register.php
+++ views/register.php
@@ -12,13 +12,23 @@
         <form action="" method="post">
             <label for="nome">Nome</label>
             <input type="text" name="nome" id="nome" required>
-
+ 
             <label for="email">Email</label>
             <input type="email" name="email" id="email" required>
-
+ 
             <label for="senha">Senha</label>
             <input type="password" name="senha" id="senha" required>
+ 
+            <label for="perfil">Perfil</label>
+            <select name="perfil" id="perfil">
+                <option value="admin">Admin</option>
+                <option value="gestor">Gestor</option>
+                <option value="colaborador">Colaborador</option>
+            </select>
+ 
+            <button type="submit">Cadastrar</button>
         </form>
+        <a href="">Voltar ao Login</a>
     </div>
 </body>
 </html>
